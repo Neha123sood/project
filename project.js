465b0297fc860688cc73b4c18be6db683a576771
Neha123sood <106972910+Neha123sood@users.noreply.github.com>
2022-06-07 13:22:41 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,10 @@
+$(".slot2handle").click(function(){
+    $(".slot2").fadeIn();
+    $(".slot1").fadeOut();
+
+});
+$(".slot1handle").click(function(){
+    $(".slot1").fadeIn();
+    $(".slot2").fadeOut();
+
+});
